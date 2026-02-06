4a1b31f2f0f2bfd99a8ed61a05d094ff600df41b
DownTrigger <andrew.ruzavin@gmail.com>
2026-02-06 19:55:00 +0700

    chore: prepare for programmatic layout

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Андрей Рузавин on 6/2/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
